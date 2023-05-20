3c9f6d5d1c3e572d284398548ed3c49a036c5d51
= <=>
2023-05-20 13:27:55 -0300

    Primeiro commint

---
+++ js/database.js
@@ -0,0 +1,23 @@
+export const posts = [
+  {
+    Titulo: "Post 1",
+    Descricao: "Exemplo Post 1",
+    Data: "01/01/2023",
+    Descricao:
+      "Post 1 ipsum dolor sit amet consectetur adipisicing elit. Eum, unde sit. Tempore nobis cumque quidem dolore blanditiis provident quisquam, omnis possimus voluptas hic excepturi architecto facilis quas a explicabo ullam.",
+  },
+  {
+    Titulo: "Post 2",
+    Descricao: "Exemplo Post 2",
+    Data: "01/02/2023",
+    Descricao:
+      "Post 2 ipsum dolor sit amet consectetur adipisicing elit. Eum, unde sit. Tempore nobis cumque quidem dolore blanditiis provident quisquam, omnis possimus voluptas hic excepturi architecto facilis quas a explicabo ullam.",
+  },
+  {
+    Titulo: "Post 3",
+    Descricao: "Exemplo Post 3",
+    Data: "01/03/2023",
+    Descricao:
+      "Post 3 ipsum dolor sit amet consectetur adipisicing elit. Eum, unde sit. Tempore nobis cumque quidem dolore blanditiis provident quisquam, omnis possimus voluptas hic excepturi architecto facilis quas a explicabo ullam.",
+  },
+];
